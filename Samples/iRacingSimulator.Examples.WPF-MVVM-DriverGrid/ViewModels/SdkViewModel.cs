3927c4ce320819e1e0005196bbe4e5f44580ba27
Jordy van Eijk <j.vaneijk@effytool.nl>
2020-02-26 11:53:38 +0100

    fixed build error

--- Samples/iRacingSimulator.Examples.WPF-MVVM-DriverGrid/ViewModels/SdkViewModel.cs
+++ Samples/iRacingSimulator.Examples.WPF-MVVM-DriverGrid/ViewModels/SdkViewModel.cs
@@ -8,19 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using iRacingSimulator.Examples.WPF_MVVM_DriverGrid.Annotations;
 
+//using iRacingSimulator.Examples.WPF_MVVM_DriverGrid.Annotations;
+
 namespace iRacingSimulator.Examples.WPF_MVVM_DriverGrid.ViewModels
 {
-    public abstract class SdkViewModel : INotifyPropertyChanged
-    {
-        public abstract void OnSessionInfoUpdated(SessionInfo info, double updateTime);
-        public abstract void OnTelemetryUpdated(TelemetryInfo info, double updateTime);
-        public event PropertyChangedEventHandler PropertyChanged;
+	public abstract class SdkViewModel : INotifyPropertyChanged
+	{
+		public abstract void OnSessionInfoUpdated(SessionInfo info, double updateTime);
+		public abstract void OnTelemetryUpdated(TelemetryInfo info, double updateTime);
+		//public event PropertyChangedEventHandler PropertyChanged;
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
+		//[NotifyPropertyChangedInvocator]
+		//protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		//{
+		//    var handler = PropertyChanged;
+		//    if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		//}
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var handler = PropertyChanged;
+			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
 }
